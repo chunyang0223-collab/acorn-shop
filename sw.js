cb9e545ce204af4422ade1a7cde77e00c5faf2f5
chunyang0223-collab <chunyang0223@gmail.com>
2026-03-01 18:16:57 +0900

    Update sw.js
    관리자 모드 오류 수정

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* =====================================================
    🌰 도토리 상점 v5 — Service Worker
    ===================================================== */
-const CACHE_NAME = 'acorn-shop-v17';
+const CACHE_NAME = 'acorn-shop-v18';
 const CACHE_URLS = [
   './index.html',
   './manifest.json',
