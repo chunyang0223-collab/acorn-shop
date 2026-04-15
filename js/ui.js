af5bd6a3d823341651aba183e9c820906d003ac1
chunyang0223-collab <chunyang0223@gmail.com>
2026-04-16 03:13:08 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -60,7 +60,8 @@ async function initAppUI() {
   }
   // 탭바 드래그 스크롤 초기화 (PC 웹 대응)
   setTimeout(() => {
-    initTabBarDragScroll(document.getElementById('userTabScroll') || document.getElementById('userTabBar'));
+    const userWrap = document.querySelector('#userMode .tab-bar-wrap');
+    initTabBarDragScroll(userWrap || document.getElementById('userTabBar'));
     initTabBarDragScroll(document.getElementById('adminTabBar'));
   }, 300);
 }
